eb4026fed13c4acfb8617023ab175b4c7c3781ab
Rusnietis <tskridla@yahoo.com>
2024-01-29 20:22:05 +0200

    Atnaujinta 01.29

---
+++ r-login/src/App.jsx
@@ -0,0 +1,9 @@
+import { RouterProvider } from './Contexts/Router';
+import './Style/app.scss';
+
+export default function App() {
+  return (
+    <RouterProvider>
+    </RouterProvider>
+  );
+}
---
+++ r-login/src/Pages/Fruits/Index.jsx
@@ -0,0 +1,11 @@
+import TopNav from '../TopNav';
+
+export default function Index() {
+    return (
+        
+        <div>
+            <TopNav />
+            <h1>Fruits List</h1>
+        </div>
+    );
+}
