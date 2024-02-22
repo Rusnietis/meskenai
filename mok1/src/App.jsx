785d0ecd28e0d091f85d063ca3575c6356a03167
Rusnietis <tskridla@yahoo.com>
2024-02-22 08:32:32 +0200

    Atnaujinta

---
+++ mok1/src/App.jsx
@@ -0,0 +1,30 @@
+
+import './App.scss';
+import './buttons.scss';
+import './form.scss';
+import { useEffect, useState } from 'react';
+
+
+
+export default function App() {
+    // const [cat, setCat] = useState('');
+    // const [dog, setDog] = useState('');
+    // const [bird, setBird] = useState('');
+
+
+
+    return (
+        <div className="App">
+            <header className="App-header">
+                <h1>CRUD</h1>
+                <div className="form">
+                    
+                   
+                    
+
+                </div>
+
+            </header>
+        </div>
+    );
+}
